5446f04369acb22edf16de706618b51bea137d76
Yusuf <Yusufsalih4@gmail.com>
2022-04-16 06:32:53 +0530

    design number generator and edits to backend code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val autostart: Button = findViewById(R.id.btnstart)
         val txtView: TextView = findViewById(R.id.txthello)
-        var start: Boolean = true
+        val start = true
 
         autostart.setOnClickListener()
         {
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
                 val luckyNumber = (1..6).random()
                 txtView.text = luckyNumber.toString()
 
-                start=false
+                //start=false
                 autostart.text="ROLL"
 
 
